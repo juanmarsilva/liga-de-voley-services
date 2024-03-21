2e79a7d3664c0e48621abdd16da500e5ada1a93b
Juan Martin Silva <juanmartinsilva74@gmail.com>
2024-03-21 19:31:42 -0300

    create user role guard

---
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -0,0 +1,44 @@
+import {
+    BadRequestException,
+    CanActivate,
+    ExecutionContext,
+    ForbiddenException,
+    Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+import { Observable } from 'rxjs';
+
+import { META_ROLES } from '../../decorators';
+import { User } from '../../entities';
+
+@Injectable()
+export class UserRoleGuard implements CanActivate {
+    constructor(private readonly reflector: Reflector) {}
+
+    canActivate(
+        context: ExecutionContext,
+    ): boolean | Promise<boolean> | Observable<boolean> {
+        const validRoles: Array<string> = this.reflector.get(
+            META_ROLES,
+            context.getHandler(),
+        );
+
+        if (!validRoles) return true;
+
+        if (validRoles.length === 0) return true;
+
+        const req = context.switchToHttp().getRequest();
+        const user = req.user as User;
+
+        if (!user) throw new BadRequestException('User not found');
+
+        for (const role of user.roles) {
+            if (validRoles.includes(role)) return true;
+        }
+
+        throw new ForbiddenException(
+            `User ${user.clubName} need a valid role: [${validRoles}]`,
+        );
+    }
+}
